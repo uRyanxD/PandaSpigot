2b1697dc77b189d51429857cf54a8ea085a5c68f
uRyanxD <familiarodrigues123ro@gmail.com>
2024-01-28 14:22:47 -0300

    work

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,10 +5,15 @@ set -e
 basedir="$(cd "$1" && pwd -P)"
 gitcmd="git -c commit.gpgsign=false"
 
-($gitcmd submodule update --init --recursive && ./scripts/remap.sh "$basedir" && ./scripts/decompile.sh "$basedir" && ./scripts/init.sh "$basedir" && ./scripts/applyPatches.sh "$basedir") || (
-    echo "Failed to build Paper"
-    exit 1
-) || exit 1
+$gitcmd submodule update --init --recursive
+
+if [ "$2" == "--setup" ] || [ "$2" == "--jar" ]; then
+    ./scripts/remap.sh "$basedir
+    ./scripts/decompile.sh "$basedir"
+    ./scripts/init.sh "$basedir"
+fi
+./scripts/applyPatches.sh "$basedir" "$2"
+
 if [ "$2" == "--jar" ]; then
     ./gradlew build && ./scripts/paperclip.sh "$basedir"
 fi
