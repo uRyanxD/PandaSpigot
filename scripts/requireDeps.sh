0373d8d089a599881a182b0b1bdae25266143370
uRyanxD <familiarodrigues123ro@gmail.com>
2024-01-28 13:28:05 -0300

    ops

--- scripts/requireDeps.sh
+++ scripts/requireDeps.sh
@@ -13,7 +13,7 @@ if [ -z "${1:-}" ]; then
     _is_dep_available patch
     _is_dep_available curl
 
-    _is_dep_available javac "was not found; you can download the JDK from https://adopium.net/ or via your package manager"
+    _is_dep_available javac "was not found; you can download the JDK from https://adoptium.net/ or via your package manager"
     _is_dep_available jar "was not found; you can download the JDK from https://adoptium.net/ or via your package manager"
 else
     # Require all dependencies provided.
